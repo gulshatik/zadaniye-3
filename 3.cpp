d287b1b46dd9a18fe76a93e4dc128f78f9e0b92b
gulshatik <91495478+gulshatik@users.noreply.github.com>
2021-10-21 11:13:41 +0500

    Update 3.cpp

--- 3.cpp
+++ 3.cpp
@@ -3,18 +3,22 @@ using namespace std;
 
 void prostiye_mnozh(int n)
 {
+	if (n < 2)
+		return;
 	int mnozh = 2;
 	while (n % mnozh != 0) {
 		mnozh++;
 	}
 	cout << mnozh << " ";
-	prostiye_mnozh(n / mnozh);
+	if (n > 2)
+		prostiye_mnozh(n / mnozh);
 	return;
 }
 
 int main()
 {
-	int N;cin >> N;
+	int N;
+	cin >> N;
 	prostiye_mnozh(N);
 	return 0;
 }
